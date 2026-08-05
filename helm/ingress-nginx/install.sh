dee405e35d759a63d2694ed17f9e26b1d596bca6
rahulltelkar <rahul.friends262@gmail.com>
2026-08-05 10:46:39 +0000

    add nginx-ingress

---
+++ helm/ingress-nginx/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm upgrade --install ingress-nginx \
+ingress-nginx/ingress-nginx \
+-n ingress-nginx \
+--create-namespace \
+-f values.yaml
